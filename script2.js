8f5a17965b3bfb3013cba597b6addd95809c83de
SimosaOmiya <43493170+SimosaOmiya@users.noreply.github.com>
2021-10-02 17:35:00 +0900

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,29 @@
+function escapeHTML(string){
+    return string.replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, "&#x27;");
+}
+window.onload = function(){
+    submit = document.getElementById("urlsend");
+    submit.addEventListener("click", () => {
+        for(let i=1;i<=8;i++){
+            url = escapeHTML(document.getElementById(`url${i}`).value);
+            url_embed = url.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/").replace("https://youtu.be/","https://www.youtube.com/embed/")
+            iframe = document.getElementById(`iframe${i}`);
+            iframe.setAttribute("src", url_embed + "?autoplay=1&mute=1");
+            if(url != ""){
+                sessionStorage.setItem(`iframe${i}`, url)
+            }
+        }
+        // console.log(sessionStorage.getItem("iframe1"))
+    });
+    storageAccess = document.getElementById("storageAccess")
+    storageAccess.addEventListener("click", () => {
+    for(let i=1;i<=8;i++){
+        document.getElementById(`url${i}`).value = sessionStorage.getItem(`iframe${i}`)
+    }
+    
+})  
+};
